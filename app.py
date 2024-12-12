b8193f05fc76171777429e079589ff8953f7759d
YunHa <pgup10753@gmail.com>
2024-12-12 10:53:54 +0900

    HG-8 feat: RAG - load documents 구현

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from crawler.healthcare_crawlers import AMCMealTherapyCrawler, SSHDiabetesCrawler
 from data_loader.data_saver import JsonSaver
+from data_loader.structured_data_loader import JsonLoader
+from preprocessor.structured_data import json_to_langchain_doclist
 
 from dotenv import load_dotenv
 import os
@@ -19,12 +21,26 @@ def crawl_and_save(crawler, save_path, force_crawl=False, **kwargs):
     json_saver.save(save_path, articles)
     print(f"저장 완료: {save_path}")
 
-def crawl_and_update(openai_api_key:str, force_crawl:bool):
+def crawl_and_update(crawl_tasks, force_crawl:bool):
     """
     실행할 크롤러를 명시, res의 json문서들을 업데이트
     main에서 호출됨
     """
-    tasks = [
+
+    for task in crawl_tasks:
+        crawl_and_save(
+            task["crawler"],
+            task["save_path"],
+            force_crawl=force_crawl,
+            **task["kwargs"]
+        )
+
+def main():
+    load_dotenv()
+    openai_api_key = os.getenv('OPENAI_API_KEY')
+
+    # RAG 0. Crawl Data
+    crawl_tasks = [
         {
             "crawler": AMCMealTherapyCrawler,
             "save_path": './res/amc-mealtherapy.json',
@@ -36,20 +52,15 @@ def crawl_and_update(openai_api_key:str, force_crawl:bool):
             "kwargs": {"api_key": openai_api_key}
         }
     ]
-
-    for task in tasks:
-        crawl_and_save(
-            task["crawler"],
-            task["save_path"],
-            force_crawl=force_crawl,
-            **task["kwargs"]
-        )
-
-def main():
-    load_dotenv()
-    openai_api_key = os.getenv('OPENAI_API_KEY')
-   
-    crawl_and_update(openai_api_key, force_crawl=False)
+    # crawl_and_update(crawl_tasks, force_crawl=False) 
+    
+    # RAG 1. Load Data
+    json_doc_paths = [crawler['save_path'] for crawler in crawl_tasks]
+    json_loader = JsonLoader()
+    documents = []
+    for path in json_doc_paths:
+        json_doc = json_loader.load(path)
+        documents += json_to_langchain_doclist(json_doc)
 
 if __name__ == "__main__":
     main()
---
+++ data_loader/base_data_loader.py
@@ -0,0 +1,6 @@
+class DataLoader:
+    def __init__(self):
+        pass
+    
+    def load(self, path):
+        raise NotImplementedError("Subclasses should implement this method.")
